9fd692254f9a2be46d86c620908c847fe83a68e9
League Student <league@iMac-9.attlocal.net>
2019-09-01 17:25:52 -0700

    i did wackAMole

--- src/_11_whack_a_mole/wackAMole.java
+++ src/_11_whack_a_mole/wackAMole.java
@@ -14,22 +14,25 @@ import javax.swing.JPanel;
 public class wackAMole implements ActionListener {
 	public static void main(String[] args) {
 		Random randy = new Random();
-	new wackAMole().createUI(randy.nextInt());
+	new wackAMole().createUI(randy.nextInt(24));
 
 }
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	
+	JButton moleButton = new JButton("mole");
 	
 	private void createUI(int x) {
 	frame.add(panel);
 	frame.setSize(250, 300);
 
+	
 
-	for(int i = 0; i<24;i++) {
+
+	for(int i = 0; i<23;i++) {
 		JButton button = new JButton();
 		button.addActionListener(this);
 		panel.add(button);
+		panel.add(moleButton);
 	}
 	frame.setVisible(true);
 	
