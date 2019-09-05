6c3f849e52dd255585c8b297456c489373e1be2a
League Student <league@iMac-22.attlocal.net>
2019-09-04 19:55:39 -0700

    i

---
+++ src/_01_nasty_surprise/nastySuprise.java
@@ -0,0 +1,42 @@
+package _01_nasty_surprise;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+
+public class nastySuprise implements ActionListener {
+	public static void main(String[] args) {
+		new nastySuprise().createUI();
+	}
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
+	private void createUI() {
+	frame.add(panel);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	panel.add(button1);
+	panel.add(button2);
+	button1.setText("trick");
+	button2.setText("treat");
+	button1.addActionListener(this);
+	button2.addActionListener(this);
+	frame.pack();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource==button1) {
+			
+		}
+	}
+}
--- src/_10_pig_latin/PigLatinTranslator.java
+++ src/_10_pig_latin/PigLatinTranslator.java
@@ -14,14 +14,15 @@ public static void main(String[] args) {
 	new PigLatinTranslator().createUI();
 }
 JButton middleButton = new JButton();
-JTextField rightBox = new JTextField(50);
-JTextField leftBox = new JTextField(50);
+JTextField rightBox = new JTextField(25);
+JTextField leftBox = new JTextField(25);
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 
 private void createUI() {
 frame.add(panel);
 frame.setVisible(true);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 panel.add(leftBox);
 panel.add(middleButton);
 panel.add(rightBox);
@@ -35,13 +36,14 @@ frame.pack();
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	String s = leftBox.getText(); 
-	String p= PigLatin.pigWord(s);
-	rightBox.setText(p);
-	System.out.println(s);
-	System.out.println(p);
-}}
+String s = leftBox.getText();
+String p = PigLatin.pigWord(s);
+rightBox.setText(p);
+System.out.println(s);
+System.out.println(p);
 
+
+}}
 
 
 
