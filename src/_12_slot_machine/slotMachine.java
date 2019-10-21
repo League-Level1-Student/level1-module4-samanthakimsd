b838ee0d992f47c01212711e881d7020aeee2d23
League Student <league@iMac.attlocal.net>
2019-10-20 17:28:17 -0700

    slot machine

--- src/_12_slot_machine/slotMachine.java
+++ src/_12_slot_machine/slotMachine.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -20,7 +21,9 @@ public static void main(String[] args) {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 JButton button = new JButton("spin");
-
+JLabel label1;
+JLabel label2;
+JLabel label3;
 private void createUI() {
 	// TODO Auto-generated method stub
 	frame.add(panel);
@@ -30,7 +33,32 @@ private void createUI() {
 	frame.setVisible(true);
 	button.addActionListener(this);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	try {
+		 label1 = createLabelImage("cherry.png"); 
+		panel.add(label1);
+	} catch (MalformedURLException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
 	
+	}
+
+	try {
+		 label2 = createLabelImage("lemon.jpg");
+		panel.add(label2);
+	} catch (MalformedURLException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	
+	try {
+		 label3 = createLabelImage("Orange-512.png");
+		panel.add(label3);
+	} catch (MalformedURLException e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
+	}
+	
+	frame.pack();
 }
 
 private JLabel createLabelImage(String fileName) throws MalformedURLException{
@@ -49,26 +77,57 @@ return imageLabel;
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
+	String pic = "";
+	panel.removeAll();
+	Random r = new Random();
+	int i = r.nextInt(3);
+	int b = r.nextInt(3);
+	int c = r.nextInt(3);
 	
+	if (i == 0 ) {
+		pic = "cherry.png";
+	}
+	if (b == 0) {
+		pic = "Orange-512.png";
+	}
+	if (b == 0) {
+		pic = "lemon.jpg";
+	}
+	if (i== 1 ) {
+		pic = "lemon.jpg";
+	}
+	if (b == 1) {
+		pic = "cherry.png";
+	}
+	if (b == 1) {
+		pic = "Orange-512.png";
+	}
+	if(i == 2) {
+		pic = "Orange-512.png";
+	}
+	if (b == 2) {
+		pic = "lemon.jpg";
+	}	
+	if (b == 2) {
+		pic = "cherry.png";
+	}	
 	try {
-		JLabel label = createLabelImage("cherry.png"); 
-		panel.add(label);
+		label1 = createLabelImage(pic);
+		panel.add(label1);
 	} catch (MalformedURLException e1) {
 		// TODO Auto-generated catch block
 		e1.printStackTrace();
 	}
-
 	try {
-		JLabel label = createLabelImage("lemon.jpg");
-		panel.add(label);
+		 label2 = createLabelImage(pic);
+		panel.add(label2);
 	} catch (MalformedURLException e1) {
 		// TODO Auto-generated catch block
 		e1.printStackTrace();
 	}
-	
 	try {
-		JLabel label = createLabelImage("Orange-512.png");
-		panel.add(label);
+		 label3 = createLabelImage(pic);
+		panel.add(label3);
 	} catch (MalformedURLException e1) {
 		// TODO Auto-generated catch block
 		e1.printStackTrace();
